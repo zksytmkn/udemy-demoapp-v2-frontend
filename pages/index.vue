99dab44d41c65fb3a1eb98604450d3b576325f03
zksytmkn <zksytmkn4869@gmail.com>
2022-10-30 21:53:34 +0900

    Get Users Table

--- pages/index.vue
+++ pages/index.vue
@@ -1,24 +1,50 @@
 <template>
   <div>
-    <button type="button" name="button" @click="getMsg">
-      RailsからAPIを取得する
-    </button>
-    <div v-for="(msg, i) in msgs" :key="i">
-      {{ msg }}
-    </div>
+    <h2>Usersテーブルの取得</h2>
+    <table v-if="users.length">
+      <thead>
+        <tr>
+          <th>id</th>
+          <th>name</th>
+          <th>email</th>
+          <th>created_at</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(user, i) in users" :key="`user-${i}`">
+          <td>{{ user.id }}</td>
+          <td>{{ user.name }}</td>
+          <td>{{ user.email }}</td>
+          <td>{{ dateFormat(user.created_at) }}</td>
+        </tr>
+      </tbody>
+    </table>
+
+    <div v-else>ユーザーが取得できませんでした</div>
   </div>
 </template>
 
 <script>
 export default {
-  data() {
-    return {
-      msgs: [],
-    };
+  // asyncData => コンポーネントのデータを表示する前に実行されるメソッド
+  // async => promiseを返す(非同期処理の結果を表示するオブジェクトのこと)
+  // await => promiseを返す迄JavaScriptを待機させる
+  async asyncData({ $axios }) {
+    let users = [];
+    await $axios.$get("/api/v1/users").then((res) => (users = res));
+      .then(res => (users = res))
+    return { users };
   },
-  methods: {
-    getMsg() {
-      this.$axios.$get("/api/v1/hello").then((res) => this.msgs.push(res));
+  // 算出プロパティ => 計算したデータを返す関数のこと
+  computed: {
+    dateFormat() {
+      return (date) => {
+        const dateTimeFormat = new Intl.DateTimeFormat("ja", {
+          dateStyle: "medium",
+          timeStyle: "short",
+        });
+        return dateTimeFormat.format(new Date(date));
+      };
     },
   },
 };
